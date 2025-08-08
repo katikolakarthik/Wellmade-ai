1bdd80c0a2051f8e8fc4743bf4b428909ca56450
KATIKOLA KARTHIK <154609029+katikolakarthik@users.noreply.github.com>
2025-08-08 19:10:05 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 // Configuration for different environments
 const config = {
   development: {
-    apiUrl: 'https://welmed.vercel.app/api'
+    apiUrl: 'https://newww-peach.vercel.app/api'
   },
   production: {
-    apiUrl: 'https://welmed.vercel.app/api' // Relative URL for production (same domain)
+    apiUrl: 'https://newww-peach.vercel.app/api' // Relative URL for production (same domain)
   }
 };
 
